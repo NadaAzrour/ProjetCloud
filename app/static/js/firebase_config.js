6f94c9fdecc1caa8e549d80d8a1ba96f5395b6cb
Benhadia Meryem <meryembenhadia@gmail.com>
2025-11-25 01:27:55 +0100

    ajout de firebase performance

--- app/static/js/firebase_config.js
+++ app/static/js/firebase_config.js
@@ -1,21 +1,20 @@
-// static/js/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
+// 1. AJOUTE CET IMPORT
+import { getPerformance } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-performance.js";
 
-// 🔹 Remplace par ta config Firebase
 const firebaseConfig = {
+  // ... (ne touche pas au reste)
   apiKey: "AIzaSyAo4rKEUQw0SGTk9vNDUu9x61yFDJbSFyo",
-  authDomain: "to-do-list-cloud-7fd86.firebaseapp.com",
-  projectId: "to-do-list-cloud-7fd86",
-  storageBucket: "to-do-list-cloud-7fd86.firebasestorage.app",
-  messagingSenderId: "918724312603",
-  appId: "1:918724312603:web:a5e998c94cfc93c7fc42be",
-  measurementId: "G-GETM6FF949"
+  // ...
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+// 2. AJOUTE CETTE LIGNE À LA FIN
+const perf = getPerformance(app);
 
 export { auth, db };
